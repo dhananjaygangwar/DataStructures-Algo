b717a641a32e6aa9388284c9c9737f12189de2e0
Dhananjay Gangwar <kannu.2097@gmail.com>
2025-10-14 22:24:29 +0530

    map way

--- Problems/TwoSum.cpp
+++ Problems/TwoSum.cpp
@@ -1,22 +1,59 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
+// vector<int> twoSum(vector<int> &nums, int target)
+// {
+//     int n = nums.size();
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = i + 1; j < n; j++)
+//         {
+//             if (nums[i] + nums[j] == target)
+//             {
+//                 return {i, j};
+//             }
+//         }
+//     }
+//     return {};
+// }
 vector<int> twoSum(vector<int> &nums, int target)
 {
-    int n = nums.size();
-    for (int i = 0; i < n; i++)
+    unordered_map<int, int> num_map; 
+    for (int i = 0; i < nums.size(); i++)
     {
-        for (int j = i + 1; j < n; j++)
+        int complement = target - nums[i];
+        
+        if (num_map.find(complement) != num_map.end()) 
         {
-            if (nums[i] + nums[j] == target)
-            {
-                return {i, j};
-            }
+            return {num_map[complement], i}; 
         }
+        
+        num_map[nums[i]] = i; 
     }
-    return {};
+    
+    return {}; 
 }
+// int main()
+// {
+//     vector<int> nums = {2, 7, 11, 15};
+//     int target = 9;
+
+//     vector<int> result = twoSum(nums, target);
+
+//     if (!result.empty())
+//     {
+//         cout << "Indices at: " << result[0] << " " << result[1] << endl;
+//     }
+//     else
+//     {
+//         cout << "no solutions found." << endl;
+//     }
+
+//     return 0;
+// }
+
 
 int main()
 {
@@ -31,7 +68,7 @@ int main()
     }
     else
     {
-        cout << "no solutions found." << endl;
+        cout << "No solutions found." << endl;
     }
 
     return 0;
