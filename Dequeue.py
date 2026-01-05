243dd4669f84db7451fbab071cb6f8f99d3b45b1
Dhananjay Gangwar <kannu.2097@gmail.com>
2026-01-05 13:17:58 +0530

    Dequeue

---
+++ Dequeue.py
@@ -0,0 +1,33 @@
+class DeQue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return len(self.items) == 0
+    
+    def insertAtEnd(self, value):
+        self.items.append(value)
+
+    def deleteAtFront(self):
+        if(self.isEmpty()):
+            print("Queue is Empty")
+        else:
+            return self.items.pop(0)
+    
+    def insertAtFront(self, value):
+        self.items.insert(0, value)
+
+    def deleteAtEnd(self):
+        if(self.isEmpty()):
+            print("Queue is Empty")
+        else:
+            return self.items.pop()
+
+dq = DeQue()
+dq.insertAtEnd(10)
+dq.insertAtFront(20)
+dq.insertAtEnd(30)
+dq.insertAtEnd(40)
+dq.insertAtFront(50)
+
+
