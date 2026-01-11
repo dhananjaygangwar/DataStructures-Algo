bd5d1696290877bca255822b0fe41e808d22d7a2
Dhananjay Gangwar <kannu.2097@gmail.com>
2026-01-11 12:22:00 +0530

    test commit for CQ

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
 using namespace std;
 
-void printHello(){
-    cout << "Hello World\n";
-}
-
 int main(){
-    printHello();
-    printHello();
-    printHello();
-    printHello();
+    
     return 0;
 }
 
