90aa3d3d749ebdabcd60b733806c3d51e57e7e79
Dhananjay Gangwar <kannu.2097@gmail.com>
2026-01-20 17:38:29 +0530

    js arrays are dynamic

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,9 @@
-let prompt = require('prompt-sync')();
-let a = prompt("Enter a num: ")
-process.stdout.write(a);
+const arr = new Array(3);
+
+arr[0] = 10;
+arr[1] = 20;
+arr[2] = 30;
+arr[3] = 40;
+
+arr[10] = 100;
+console.log(arr);
