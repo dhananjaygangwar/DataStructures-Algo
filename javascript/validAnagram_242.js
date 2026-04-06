cc4cf3a19f65df6cf95bac004ad19715219297f4
Dhananjay Gangwar <kannu.2097@gmail.com>
2026-04-06 11:44:19 +0530

    valid anagram hash map way added

--- javascript/validAnagram_242.js
+++ javascript/validAnagram_242.js
@@ -1,11 +1,32 @@
+//sorting way (29ms)
+// const isAnagram = (t,s) => {
+//     if(s.length !== t.length){
+//         return false;
+//     }
+//     let sSort = s.split('').sort().join();
+//     let tSort = t.split('').sort().join();
+//     return sSort == tSort;
+// };
+
+// Hash map()
 const isAnagram = (t,s) => {
-    if(s.length !== t.length){
+    if (s.length !== t.length){
         return false;
     }
-    let sSort = s.split('').sort().join();
-    let tSort = t.split('').sort().join();
-    return sSort == tSort;
-};
+
+    const countS = {};
+    const countT = {};
+    for (let i = 0; i < s.length; i++) {
+        countS[s[i]] = (countS[s[i]] || 0) + 1;
+        countT[t[i]] = (countT[t[i]] || 0) + 1;        
+    }
+    for(const key in countS){
+        if (countS[key] !== countT[key]){
+            return false;
+        }
+    }
+    return true;    
+}
 
 const s = "anagram"
 const t = "nagaram"
